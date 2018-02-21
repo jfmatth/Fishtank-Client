3c375d4a712fc8021abc9c2a8dd316751ffdedf6
John Matthew <john@compunique.com>
2018-02-21 07:04:24 -0800

    setup some default settings and making the configmanager read the defaults

--- config.py
+++ config.py
@@ -1,24 +1,55 @@
 import pathlib
 import os
 import platform
+import json
 
-
+basepath = pathlib.Path(os.path.abspath(os.path.dirname(__file__) ) )
 
 class Config(object):
     # base meta class for all config types that return configuration info
     # this will derive all other classes based on Operating system
 
     def __init__(self):
+        # common init routine, should pull down any setup necessary from server
+        self.settings = {}
+        self.defaultsfile = "defaults.json"
+
+        self._Setup()
+        self._LoadDefaults()
+
+    def __getattr__(self, name):
+        # fallback if we are looking from something from settings
+        return self.settings.get(name, None)
+
+    def _Setup(self):
+        # we set any local stuff here so as not to have to override __init__
         pass
 
+    def _LoadDefaults(self):
+        # load a default file with settings?
+        print("Loading defaults from %s" % self.defaultsfile)
+
+        if self.defaultsfile and pathlib.Path(self.defaultsfile).exists():
+            self.settings = json.loads(open(str(self.defaultsfile)).read() )
+        else:
+            print("no settings file %s exists" % self.defaultsfile)
+
     def ArchivePath(self):
-        return pathlib.Path(os.path.abspath(__file__) ) + "archives"
+        return basepath / "archives"
 
     def Platform(self):
         return platform.system()
 
+    def RootFolders(self):
+        # return a list of root folders to backup, these might be:
+        # C:/users/, /home, etc, depending on the platform
+        return self.settings.get("RootFolders", None)
+
+
 class WindowsConfig(Config):
-    pass
+
+    def _Setup(self):
+        self.defaultsfile = "defaultWindows.json"
 
 class OSXConfig(Config):
     pass
@@ -40,10 +71,13 @@ def ConfigManager():
     if p == "Linux":
         return LinuxConfig()
     
-    assert 0, "Cannot deterimine platform" + p
+    assert 0, "Platform %s not supported" % p
+
+
 
 if __name__ == "__main__":
     # 
     x = ConfigManager()
-    print(x.Platform())
+    print("Platform: %s" % x.Platform())
+    print("ArchivePath: %s" % x.ArchivePath())
 
