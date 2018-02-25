b222f7cdf276495357f08e5abaf686d3091a023c
John Matthew <john@compunique.com>
2018-02-25 11:17:35 -0800

    Adjust test client to account for ConfigManager()

--- testclient.py
+++ testclient.py
@@ -1,25 +1,29 @@
 import json
 import logging
-from jsondict import jsondict
+# from jsondict import jsondict
 
 from backupmanager import BackupManager
+from config import ConfigManager
 
 logger = logging.getLogger(__name__)
 
 def stopcallback(instance):
     print (instance.CurrentFile)
     print (instance.archive.size)
+    print (instance.zi)
     return False
 
-config = jsondict(filename="config.json")
-print (config)
+# config = jsondict(filename="config.json")
+# print (config)
+
+config = ConfigManager()
 
 # backup = BackupManager( s["archivepath"] )
 backup = BackupManager( config )
 
-backup.drives = config['drives']
-backup.dirglob = config['dirglob']
-backup.fileglob = config['fileglob']
+# backup.drives = config['drives']
+# backup.dirglob = config['dirglob']
+# backup.fileglob = config['fileglob']
 
 backup._stop = stopcallback
 
