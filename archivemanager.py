205bbf23d48d7fe170a0230039d46ff46beed5f6
John Matthew <john@compunique.com>
2018-02-25 11:16:01 -0800

    Move all DB functions to it's own module db.py

--- archivemanager.py
+++ archivemanager.py
@@ -4,49 +4,10 @@ import logging
 import os
 import uuid
 
-import peewee
-
+from db import database, Backup, File
 from config import _Config
 
 logger = logging.getLogger(__name__)
-
-"""
-DB Layout
-
-Backup
-    |
-    |--> File
-"""
-
-
-# Define the DB's we'll use in this module.
-database = peewee.SqliteDatabase(None)
-class basetable(peewee.Model):
-    class Meta:
-       database = database
-
-# Backup - represents a ZIP file backup
-class Backup(basetable):
-    fullpath = peewee.CharField(unique=True, index=True)
-    ready = peewee.BooleanField(index=True)
-    encrypted = peewee.BooleanField(index=True)
-    uploaded = peewee.BooleanField(index=True)
-
-# File - each file in the ZIP, related to that parent record.
-class File(basetable):
-    backup = peewee.ForeignKeyField(Backup)
-
-    fullpath = peewee.CharField(index=True)
-    crc = peewee.CharField(index=True)
-    filename = peewee.CharField()
-    size = peewee.IntegerField()
-    modified = peewee.DateField()
-    accessed = peewee.DateField()
-    created = peewee.DateField()
-
-    class Meta:
-        primary_key = peewee.CompositeKey("fullpath", "crc")
-
 
 class ArchiveManager():
     """
@@ -139,7 +100,6 @@ class ArchiveManager():
         self.archive = None             # current archive (zip) file being used.
         self.name = None                # current name of arhive
         self.backuprecord = None       # FK pointer
-
 
     def _newarchive(self):
         """
